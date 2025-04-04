04d2a2ea893794cce504d80ef8f615c1b06e81d7
Luck7702 <dennisch2206@gmail.com>
2025-04-05 02:12:42 +0700

    Update thescript.js

--- thescript.js
+++ thescript.js
@@ -1,72 +1,72 @@
+const DAY_IN_MS  = (86400000);    // Amount of Milliseconds in a Day
+const OFFSET     = (25200000);    // 7 Hours difference cuz of UTC +7
+
 let datestring = document.getElementById("datestring");
 let text2 = document.getElementById("textz2");
 let dateInput = document.getElementById("dateInput");
 
-let Doomsday = new Date("2025-09-08");
+let Doomsday = new Date("2025-09-08"); Doomsday -= OFFSET; 
 let doomsdayhascome = 0;
 
 function calculatedayz(Target){
+
     let x = (Math.ceil((Target - new Date()) / (1000 * 3600 * 24)));
+
     if(x <= 0 ){
-
         doomsdayhascome = 1;
-        return ("Doomsday has come");
-        
-    }
+        return ("Doomsday has come");        
+    }; doomsdayhascome = 0;
 
-    doomsdayhascome = 0;
     return String((x)+" Days");
-    
 }
 
 function calculateText(date){
 
-    let targetDate = date;
+    let targetDate = new Date(date);
 
     let day = targetDate.getDate();
-    let month = targetDate.getMonth();  // Months are 0-indexed
+    let month = targetDate.getMonth(); 
     let year = targetDate.getFullYear();
+
     const monthNames = [
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
       ];    
 
     return (`${day}` + " " + `${monthNames[month]}` + " " + `${year}`);
-
 }
 
-let bruhDate = calculatedayz(Doomsday);
-
 datestring.textContent = calculateText(Doomsday);
-text2.textContent = bruhDate
+text2.textContent      = calculatedayz(Doomsday);
 
-text2.addEventListener("mouseenter",function(){
+
+//////////////////////////////////--MAIN FUNCTION--///////////////////////////////////////////////
+
+
+text2.addEventListener("mouseenter", function(){
 
     if(doomsdayhascome == 1){
-        text2.textContent = "May God be with You";
+        text2.textContent = Math.abs(Math.ceil((Doomsday - new Date()) / (1000 * 3600 * 24)) ) + " days have passed";
         return;
     }
-    text2.textContent = "Is the same as from "  + 
-    (calculateText((new Date  ((new Date()) - ((Doomsday - new Date()) - (1000 * 3600 * 7)))))) + " to now";
-    console.log("As From : " + (new Date  ((new Date()) - ((Doomsday - new Date())- (1000 * 3600 * 7)))));
+    text2.textContent = "Same as "  +  
+    (calculateText((new Date  ((new Date()) - ((Doomsday - new Date())))))) + " until now";
+
+    console.log("As From : " + (new Date  ((new Date()) - ((Doomsday - new Date())))));
     console.log("Current Date : " + new Date());
-    console.log("Doomsday : " + new Date(Doomsday - (1000 * 3600 * 7)));
+    console.log("Doomsday : " + new Date(Doomsday));
 })
 
-text2.addEventListener("mouseleave",function(){
+text2.addEventListener("mouseleave", function(){text2.textContent = calculatedayz(Doomsday)})
 
-    text2.textContent = calculatedayz(Doomsday)
-
-})
-
-datestring.addEventListener("click",function(){
+datestring.addEventListener("click", function(){ 
 
     dateInput.showPicker();
     dateInput.addEventListener("change", function(){
         
-        text2.textContent = bruhDate;
-        Doomsday = new Date(this.value);
-        bruhDate = calculatedayz(Doomsday);
+        Doomsday = new Date(this.value); Doomsday -= OFFSET;
+        
+        bruhDate = calculatedayz(Doomsday); text2.textContent = bruhDate;
         datestring.textContent = calculateText(Doomsday);
     })
 })
